3df4ddb23dc90ce415194400320e2ba90563bee5
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-08-06 12:26:44 +0800

    update

---
+++ install.sh
@@ -0,0 +1,6 @@
+ln -s -f ~/.config/tmux/tmux.conf ~/.tmux.conf
+ln -s -f ~/.config/tmux/tmux.conf.local ~/.tmux.conf.local
+ln -s ~/.config/tmux ~/.tmux  
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source-file ~/.tmux.conf
