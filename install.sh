cda4e5d267eda3f8c277836dd89929afeebac0d2
MatchaEggTart <MatchaEggTart@hotmail.com>
2023-08-20 19:28:31 +0800

    update

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ ln -s -f ~/.config/tmux/tmux.conf ~/.tmux.conf
 ln -s -f ~/.config/tmux/tmux.conf.local ~/.tmux.conf.local
 ln -s ~/.config/tmux ~/.tmux  
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-tmux source-file ~/.tmux.conf
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# tmux source-file ~/.tmux.conf
 
 # sudo dnf install xclip -y
 # sudo dnf install xsel -y
